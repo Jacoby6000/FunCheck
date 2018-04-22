a033f12c29fb70901d2b106d89e38a8a2a88c4cb
Jacob Barber <Jacoby6000@gmail.com>
2018-04-22 18:19:04 -0400

    redundant

--- src/FunCheck/Data/Model.hs
+++ src/FunCheck/Data/Model.hs
@@ -2,7 +2,6 @@ module FunCheck.Data.Model(Pattern(..), SymbolRef, patternCata) where
 
 data Pattern
   = Lit String
-  | OneOf [Pattern]
   | IntRange Int Int
   | DecRange Double Double
   | Optional Pattern
@@ -13,15 +12,14 @@ data Pattern
 
 newtype SymbolRef = SymbolRef String
 
-patternCata :: (String -> a) -> ([a] -> a) -> (Int -> Int -> a) -> (Double -> Double -> a) -> (a -> a) -> (SymbolRef -> a) -> (a -> Maybe Int -> Maybe Int -> a) -> ([a] -> a) -> ([a] -> a) -> Pattern -> a
-patternCata litF _ _ _ _ _ _ _ _ (Lit          s) = litF s
-patternCata f patF g h i j k l m (OneOf     pats) = patF $ patternCata f patF g h i j k l m <$> pats
-patternCata _ _ intF _ _ _ _ _ _ (IntRange mn mx) = intF mn mx
-patternCata _ _ _ decF _ _ _ _ _ (DecRange mn mx) = decF mn mx
-patternCata f g h i optF j k l m (Optional   pat) = optF $ patternCata f g h i optF j k l m pat
-patternCata _ _ _ _ _ symF _ _ _ (Var        ref) = symF ref
-patternCata f g h i j k repF l m (Repeat p mn mx) = repF (patternCata f g h i j k repF l m p) mn mx
-patternCata f g h i j k l andF m (And       pats) = andF $ patternCata f g h i j k l andF m <$> pats
-patternCata f g h i j k l m orFn (Or        pats) = orFn $ patternCata f g h i j k l m orFn <$> pats
+patternCata :: (String -> a) -> (Int -> Int -> a) -> (Double -> Double -> a) -> (a -> a) -> (SymbolRef -> a) -> (a -> Maybe Int -> Maybe Int -> a) -> ([a] -> a) -> ([a] -> a) -> Pattern -> a
+patternCata litF _ _ _ _ _ _ _ (Lit          s) = litF s
+patternCata _ intF _ _ _ _ _ _ (IntRange mn mx) = intF mn mx
+patternCata _ _ decF _ _ _ _ _ (DecRange mn mx) = decF mn mx
+patternCata f g h optF i j k l (Optional   pat) = optF $ patternCata f g h optF i j k l pat
+patternCata _ _ _ _ symF _ _ _ (Var        ref) = symF ref
+patternCata f g h i j repF k l (Repeat p mn mx) = repF (patternCata f g h i j repF k l p) mn mx
+patternCata f g h i j k andF l (And       pats) = andF $ patternCata f g h i j k andF l <$> pats
+patternCata f g h i j k l orFn (Or        pats) = orFn $ patternCata f g h i j k l orFn <$> pats
 
 
