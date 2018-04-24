d798bf24c34ca71173680c76a2842ed2cfba2f5c
Jacob Barber <jacob.barber@bamtechmedia.com>
2018-04-23 20:16:00 -0400

    Recursion Schemes

--- src/FunCheck/Data/Model.hs
+++ src/FunCheck/Data/Model.hs
@@ -1,14 +1,25 @@
+{-# LANGUAGE TemplateHaskell, KindSignatures, TypeFamilies #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+
 module FunCheck.Data.Model(Pattern(..), Symbol) where
 
+import Data.Functor.Foldable
+import Data.Functor.Foldable.TH
+import Data.List (foldl')
+import Data.Functor.Identity
+
 data Pattern a
-  = Lit String
+  = Lit a
   | IntRange Int Int
   | DecRange Double Double
-  | Optional a
-  | Repeat a (Maybe Int) (Maybe Int)
-  | And [a]
-  | Or [a]
+  | Optional (Pattern a)
+  | Repeat (Pattern a) (Maybe Int) (Maybe Int)
+  | And [Pattern a]
+  | Or [Pattern a]
   | Var Symbol
-  | Let Symbol a
+  | Let Symbol (Pattern a)
 
 newtype Symbol = Symbol String
+
+
+makeBaseFunctor ''Pattern
