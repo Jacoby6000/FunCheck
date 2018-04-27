725443becf8ae4be5e23eb6b10761efaa4b15f84
Jacob Barber <jacob.barber@bamtechmedia.com>
2018-04-27 11:10:15 -0400

    generalized stuff

--- src/FunCheck/Generate.hs
+++ src/FunCheck/Generate.hs
@@ -1,10 +1,8 @@
-module FunCheck.Generate(generateData) where
+module FunCheck.Generate(cataState) where
 
-import System.Random
-import FunCheck.Model
 import Control.Monad.State.Lazy
 import Yaya.Control
 
 -- Template a -> State g a
-generateData :: (Traversable m, Recursive f m, RandomGen g) => (m a -> State g a) -> g -> f -> a
-generateData alg gen template = fst (runState (cataM alg template) gen)
+cataState :: (Traversable m, Recursive f m) => (m a -> State g a) -> g -> f -> a
+cataState alg gen fm = evalState (cataM alg fm) gen
