00640140640ad4f6627cbdf4d13ae03b6308a746
Jacob Barber <jacob.barber@bamtechmedia.com>
2018-04-27 12:45:07 -0400

    remove old comment git push

--- src/FunCheck/Generate.hs
+++ src/FunCheck/Generate.hs
@@ -3,6 +3,7 @@ module FunCheck.Generate(cataState) where
 import Control.Monad.State.Lazy
 import Yaya.Control
 
--- Template a -> State g a
 cataState :: (Traversable m, Recursive f m) => (m a -> State g a) -> g -> f -> a
 cataState alg gen fm = evalState (cataM alg fm) gen
+
+
