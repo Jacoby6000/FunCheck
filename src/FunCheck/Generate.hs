7eb2f13f5ddea0415ba34fbd0376121387269bef
Jacob Barber <Jacoby6000@gmail.com>
2018-04-27 21:23:05 -0400

    formatting

--- src/FunCheck/Generate.hs
+++ src/FunCheck/Generate.hs
@@ -21,23 +21,24 @@ instance HasGen g (Env g a) where
 cataState :: (Traversable m, Recursive f m, Monad n) => AlgebraM (StateT s n) m a -> s -> f -> n a
 cataState alg s fm = evalStateT (cataM alg fm) s
 
-monoidalStepAlgebra :: (RandomGen g, Monoid a, MonadState (Env g a) m) =>
-                       (Int    -> a) ->
-                       (Double -> a) ->
-                       (String -> a) ->
-                       (t      -> a) ->
-                       Template t a  ->
-                       m a
-monoidalStepAlgebra _ _ _ f (Lit           t) = state $ (,) (f t)
+monoidalStepAlgebra
+  :: (RandomGen g, Monoid a, MonadState (Env g a) m)
+  => (Int -> a)
+  -> (Double -> a)
+  -> (String -> a)
+  -> (t -> a)
+  -> Template t a
+  -> m a
+monoidalStepAlgebra _ _ _ f (Lit t          ) = state $ (,) (f t)
 monoidalStepAlgebra f _ _ _ (IntRange  mn mx) = f <$> randomRange (mn, mx)
 monoidalStepAlgebra _ f _ _ (DecRange  mn mx) = f <$> randomRange (mn, mx)
-monoidalStepAlgebra _ _ f _ (CharRange mn mx) = f . (:[]) <$> randomRange (mn, mx)
-monoidalStepAlgebra _ _ _ _ (Optional      a) = (\b -> if b then a else mempty) <$> randomResult
-monoidalStepAlgebra _ _ _ _ (Repeat  a mn mx) = foldMap id <$> flip replicate a <$> randomRange (mn, mx)
-monoidalStepAlgebra _ _ _ _ (And          as) = state $ (,) (foldMap id as)
-monoidalStepAlgebra _ _ _ _ (Or           as) = randomChoice as
-monoidalStepAlgebra _ _ _ _ (Var         sym) = state $ preserveInput (findWithDefault mempty sym . (view bindings))
-monoidalStepAlgebra _ _ _ _ (Let     sym   a) = state (\env -> (mempty, over bindings (insert sym a) env))
+monoidalStepAlgebra _ _ f _ (CharRange mn mx) = f . (: []) <$> randomRange (mn, mx)
+monoidalStepAlgebra _ _ _ _ (Optional a     ) = (\b -> if b then a else mempty) <$> randomResult
+monoidalStepAlgebra _ _ _ _ (Repeat a mn mx ) = foldMap id <$> flip replicate a <$> randomRange (mn, mx)
+monoidalStepAlgebra _ _ _ _ (And as         ) = state $ (,) (foldMap id as)
+monoidalStepAlgebra _ _ _ _ (Or  as         ) = randomChoice as
+monoidalStepAlgebra _ _ _ _ (Var sym        ) = state $ preserveInput (findWithDefault mempty sym . (view bindings))
+monoidalStepAlgebra _ _ _ _ (Let sym a      ) = state (\env -> (mempty, over bindings (insert sym a) env))
 
 
 preserveInput :: (a -> b) -> a -> (b, a)
--- src/Main.hs
+++ src/Main.hs
@@ -7,32 +7,31 @@ import Data.Map.Lazy
 import Yaya.Unsafe.Data
 
 main :: IO ()
-main = do { gen <- getStdGen
-          ; result <- cataState (monoidalStepAlgebra show show id id) (Env gen empty) template
-          ; putStrLn result
-          }
+main = do
+  gen    <- getStdGen
+  result <- cataState (monoidalStepAlgebra show show id id) (Env gen empty) template
+  putStrLn result
 
 template :: Fix (Template String)
-template =
-  Fix (
-    And [
-      Fix (Lit "foo"),
-      Fix (Let (Symbol "numOrChar") (Fix (Or [Fix $ IntRange 10 20, Fix $ CharRange 'a' 'z']))),
-      Fix (Var (Symbol "numOrChar")),
-      Fix (Var (Symbol "numOrChar")),
-      Fix (Var (Symbol "numOrChar")),
-      Fix (Var (Symbol "numOrChar")),
-      Fix (Var (Symbol "numOrChar")),
-      Fix (Lit "\n1: "),
-      Fix (IntRange 10 20),
-      Fix (Lit "\n2: "),
-      Fix (IntRange 10 20),
-      Fix (Lit "\n3: "),
-      Fix (IntRange 10 20),
-      Fix (Lit "\n4: "),
-      Fix (IntRange 10 20),
-      Fix (Lit "\n5: "),
-      Fix (IntRange 10 20)
+template = Fix
+  (And
+    [ Fix (Lit "foo")
+    , Fix (Let (Symbol "numOrChar") (Fix (Or [Fix $ IntRange 10 20, Fix $ CharRange 'a' 'z'])))
+    , Fix (Var (Symbol "numOrChar"))
+    , Fix (Var (Symbol "numOrChar"))
+    , Fix (Var (Symbol "numOrChar"))
+    , Fix (Var (Symbol "numOrChar"))
+    , Fix (Var (Symbol "numOrChar"))
+    , Fix (Lit "\n1: ")
+    , Fix (IntRange 10 20)
+    , Fix (Lit "\n2: ")
+    , Fix (IntRange 10 20)
+    , Fix (Lit "\n3: ")
+    , Fix (IntRange 10 20)
+    , Fix (Lit "\n4: ")
+    , Fix (IntRange 10 20)
+    , Fix (Lit "\n5: ")
+    , Fix (IntRange 10 20)
     ]
   )
 
