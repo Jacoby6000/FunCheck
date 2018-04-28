c20f1db926ec07b851b63c74838be437e408727a
Jacob Barber <Jacoby6000@gmail.com>
2018-04-28 07:12:40 -0400

    Simplifications

--- src/FunCheck/Generate.hs
+++ src/FunCheck/Generate.hs
@@ -29,20 +29,16 @@ monoidalStepAlgebra
   -> (t -> a)
   -> Template t a
   -> m a
-monoidalStepAlgebra _ _ _ f (Lit t          ) = state $ (,) (f t)
+monoidalStepAlgebra _ _ _ f (Lit t          ) = pure $ f t
 monoidalStepAlgebra f _ _ _ (IntRange  mn mx) = f <$> randomRange (mn, mx)
 monoidalStepAlgebra _ f _ _ (DecRange  mn mx) = f <$> randomRange (mn, mx)
 monoidalStepAlgebra _ _ f _ (CharRange mn mx) = f . (: []) <$> randomRange (mn, mx)
 monoidalStepAlgebra _ _ _ _ (Optional a     ) = (\b -> if b then a else mempty) <$> randomResult
 monoidalStepAlgebra _ _ _ _ (Repeat a mn mx ) = foldMap id <$> flip replicate a <$> randomRange (mn, mx)
-monoidalStepAlgebra _ _ _ _ (And as         ) = state $ (,) (foldMap id as)
+monoidalStepAlgebra _ _ _ _ (And as         ) = pure $ foldMap id as
 monoidalStepAlgebra _ _ _ _ (Or  as         ) = randomChoice as
-monoidalStepAlgebra _ _ _ _ (Var sym        ) = state $ preserveInput (findWithDefault mempty sym . (view bindings))
-monoidalStepAlgebra _ _ _ _ (Let sym a      ) = state (\env -> (mempty, over bindings (insert sym a) env))
-
-
-preserveInput :: (a -> b) -> a -> (b, a)
-preserveInput f a = (f a, a)
+monoidalStepAlgebra _ _ _ _ (Var sym        ) = uses bindings (findWithDefault mempty sym)
+monoidalStepAlgebra _ _ _ _ (Let sym a      ) = mempty <$ (bindings %= (insert sym a))
 
 randomRange :: (RandomGen g, Random r, HasGen g (Env g a), MonadState (Env g a) m) => (r, r) -> m r
 randomRange x = generator %%= randomR x
