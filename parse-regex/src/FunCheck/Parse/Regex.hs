fdaeb4db079840460a1a3c7667999e7425fcb08c
Jacob Barber <Jacoby6000@gmail.com>
2018-04-29 14:25:00 -0400

    Untested first pass regex parser

--- parse-regex/src/FunCheck/Parse/Regex.hs
+++ parse-regex/src/FunCheck/Parse/Regex.hs
@@ -1,9 +1,8 @@
-module FunCheck.Parse.Regex(Regex(..), Choose(..), SpecialChar(..), specialChar, choose) where
+module FunCheck.Parse.Regex(Regex(..), Choose(..), SpecialChar(..), specialChar, choose, regex) where
 
 import Control.Applicative
 import Data.Attoparsec.Text (Parser)
 import Data.Char
-import Data.List.NonEmpty
 
 import qualified Data.Attoparsec.Text as A
 -- import qualified Data.Attoparsec.Combinator as AC
@@ -43,61 +42,79 @@ data SpecialChar
  | NullEscape
  | OctalEscape Char
  | HexadecimalEscape Char
+ | EscapedChar Char
   deriving(Show, Eq)
 
 regex :: Parser Regex
-regex =
-  oneOf <|> notOneOf <|> captureGroup <|> special <|> or <|> and <|> optional <|> lit
+regex = allButLit <|> lit
+ where
+  notOneOf = NotOneOf <$> parseAllWithin "[^" choose "]"
+  oneOf = OneOf <$> parseAllWithin "[" choose "]"
+  captureGroup = parseWithin "(" regex ")"
+  orParser = Or <$> regex <* constParse () "|" <*> regex
+  andParser = And <$> regex <*> regex
+  opt = Optional <$> regex <* (constParse () "?")
+  special = Special <$> specialChar
+
+  allButLit = notOneOf <|> oneOf <|> captureGroup <|> special <|> orParser <|> andParser <|> opt
+  lit = Lit <$> A.manyTill A.anyChar allButLit
 
 choose :: Parser Choose
-choose =
-  chooseSpecialChar <|> chooseCharRange <|> chooseOneChar
-  where
-    chooseSpecialChar = (ChooseSpecialChar <$> specialChar)
-    chooseCharRange = (ChooseCharRange <$> A.anyChar <* constParse id "-" <*> A.anyChar)
-    chooseOneChar = (ChooseOneChar <$> A.anyChar)
+choose = chooseSpecialChar <|> chooseCharRange <|> chooseOneChar
+ where
+  chooseSpecialChar = ChooseSpecialChar <$> specialChar
+  chooseCharRange   = ChooseCharRange <$> A.anyChar <* constParse () "-" <*> A.anyChar
+  chooseOneChar     = ChooseOneChar <$> A.anyChar
 
 specialChar :: Parser SpecialChar
 specialChar =
-  whitespace <|>
-  number <|>
-  alphaNumeric <|>
-  slash <|>
-  tab <|>
-  verticalTab <|>
-  cr <|>
-  lf <|>
-  crlf <|>
-  escape <|>
-  backspace <|>
-  formFeed <|>
-  alert <|>
-  nullEscape <|>
-  octalEscape <|>
-  hexadecimalEscape
-  where
-    whitespace = constParse Whitespace "\\s"
-    number = constParse Number "\\d"
-    alphaNumeric = constParse Whitespace "\\w"
-    slash = constsParse Slash $ "\\B" :| ["\\\\"]
-    tab = constParse Tab "\\t"
-    verticalTab = constParse VerticalTab "\\v"
-    cr = constParse CR "\\r"
-    lf = constParse LF "\\n"
-    crlf = constParse CRLF "\\R"
-    escape = constParse Escape "\\e"
-    backspace = constParse Backspace "\\b"
-    formFeed = constParse  FormFeed "\\f"
-    alert = constParse Alert "\\a"
-    nullEscape = constParse NullEscape "\0"
-    octalEscape = OctalEscape <$> (constParse () "\\" *> error "not implemented")
-    hexadecimalEscape = HexadecimalEscape <$> chr <$> (constParse () "\\" *> A.hexadecimal)
+  whitespace
+    <|> number
+    <|> alphaNumeric
+    <|> slash
+    <|> tab
+    <|> verticalTab
+    <|> cr
+    <|> lf
+    <|> crlf
+    <|> escape
+    <|> backspace
+    <|> formFeed
+    <|> alert
+    <|> nullEscape
+    <|> octalEscape
+    <|> hexadecimalEscape
+    <|> escapedChar
+ where
+  whitespace        = constParse Whitespace "\\s"
+  number            = constParse Number "\\d"
+  alphaNumeric      = constParse Whitespace "\\w"
+  slash             = constParse Slash "\\B"
+  tab               = constParse Tab "\\t"
+  verticalTab       = constParse VerticalTab "\\v"
+  cr                = constParse CR "\\r"
+  lf                = constParse LF "\\n"
+  crlf              = constParse CRLF "\\R"
+  escape            = constParse Escape "\\e"
+  backspace         = constParse Backspace "\\b"
+  formFeed          = constParse FormFeed "\\f"
+  alert             = constParse Alert "\\a"
+  nullEscape        = constParse NullEscape "\0"
+  octalEscape       = OctalEscape <$> (constParse () "\\" *> error "not implemented")
+  hexadecimalEscape = HexadecimalEscape <$> chr <$> (constParse () "\\" *> A.hexadecimal)
+  escapedChar       = EscapedChar <$> (constParse () "\\" *> A.anyChar)
 
 
 constParse :: a -> String -> Parser a
 constParse a s = A.asciiCI (T.pack s) *> pure a
 
-constsParse :: a -> NonEmpty String -> Parser a
-constsParse a (h :| t) = foldr ((<|>) . constParse a) (constParse a h) t
+parseAllWithin :: String -> Parser a -> String -> Parser [a]
+parseAllWithin start parser end = constParse () start *> A.manyTill parser (constParse () end)
+
+parseWithin :: String -> Parser a -> String -> Parser a
+parseWithin start parser end = constParse () start *> parser <* constParse () end
+
+-- constsParse :: a -> NonEmpty String -> Parser a
+-- constsParse a (h :| t) = foldr ((<|>) . constParse a) (constParse a h) t
 
 
