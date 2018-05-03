9a2d0e45de53264e6fc357216914e59c021b3554
Jacob Barber <jacob.barber@bamtechmedia.com>
2018-05-02 21:17:08 -0400

    Parser work

--- parse-regex/src/FunCheck/Parse/Regex.hs
+++ parse-regex/src/FunCheck/Parse/Regex.hs
@@ -3,9 +3,10 @@ module FunCheck.Parse.Regex(Regex(..), Choose(..), SpecialChar(..), specialChar,
 import Control.Applicative
 import Data.Attoparsec.Text (Parser)
 import Data.Char
+import Data.Functor
 
 import qualified Data.Attoparsec.Text as A
--- import qualified Data.Attoparsec.Combinator as AC
+import qualified Data.Attoparsec.Combinator as AC
 import qualified Data.Text as T
 
 data Regex
@@ -16,7 +17,7 @@ data Regex
   | Special SpecialChar
   | Optional Regex
   | Or Regex Regex
-  | And Regex Regex
+  | And [Regex]
   deriving(Show, Eq)
 
 data Choose
@@ -45,25 +46,26 @@ data SpecialChar
  | EscapedChar Char
   deriving(Show, Eq)
 
+checkSeperator :: Parser Char
+checkSeperator = AC.lookAhead $ A.choice (A.char <$> ['(', '[', ']', ')', '?', '|'])
+
 regex :: Parser Regex
-regex = allButLit <|> lit
+regex = And <$> AC.manyTill parseRegexAtoms AC.endOfInput
+
+parseRegexAtoms :: Parser Regex
+parseRegexAtoms = notOneOf <|> oneOf <|> captureGroup <|> special <|> lit
  where
   notOneOf = NotOneOf <$> parseAllWithin "[^" choose "]"
   oneOf = OneOf <$> parseAllWithin "[" choose "]"
   captureGroup = parseWithin "(" regex ")"
-  orParser = Or <$> regex <* constParse () "|" <*> regex
-  andParser = And <$> regex <*> regex
-  opt = Optional <$> regex <* (constParse () "?")
   special = Special <$> specialChar
-
-  allButLit = notOneOf <|> oneOf <|> captureGroup <|> special <|> orParser <|> andParser <|> opt
-  lit = Lit <$> A.manyTill A.anyChar allButLit
+  lit = Lit <$> A.manyTill A.anyChar checkSeperator
 
 choose :: Parser Choose
 choose = chooseSpecialChar <|> chooseCharRange <|> chooseOneChar
  where
   chooseSpecialChar = ChooseSpecialChar <$> specialChar
-  chooseCharRange   = ChooseCharRange <$> A.anyChar <* constParse () "-" <*> A.anyChar
+  chooseCharRange   = ChooseCharRange <$> A.anyChar <* A.char '-' <*> A.anyChar
   chooseOneChar     = ChooseOneChar <$> A.anyChar
 
 specialChar :: Parser SpecialChar
@@ -106,15 +108,12 @@ specialChar =
 
 
 constParse :: a -> String -> Parser a
-constParse a s = A.asciiCI (T.pack s) *> pure a
+constParse a s = A.asciiCI (T.pack s) $> a
 
 parseAllWithin :: String -> Parser a -> String -> Parser [a]
-parseAllWithin start parser end = constParse () start *> A.manyTill parser (constParse () end)
+parseAllWithin start parser end = constParse () start *> A.many1 parser <* constParse () end
 
 parseWithin :: String -> Parser a -> String -> Parser a
 parseWithin start parser end = constParse () start *> parser <* constParse () end
-
--- constsParse :: a -> NonEmpty String -> Parser a
--- constsParse a (h :| t) = foldr ((<|>) . constParse a) (constParse a h) t
 
 
